48dd4fb45ee7910912daeccc79456a6719852667
jay <chenjie@olight.com>
2024-07-12 19:55:18 +0800

    xiaoo

--- dep.sh
+++ dep.sh
@@ -1,4 +1,19 @@
 pyinstaller --name "小O" --onefile --windowed --icon=xiaoo.icns run_DyberPet.py
+
+   datas=[
+        ('res/icons', 'res/icons'),
+        ('res/items', 'res/items'),
+        ('res/language', 'res/language'),
+        ('res/pet', 'res/pet'),
+        ('res/role', 'res/role'),
+        ('res/sounds', 'res/sounds')
+    ],
+    
+pyinstaller 小O.spec
+
+
+
+
 pyinstaller --name "xiaoo" --onefile --console --icon=xiaoo.icns run_DyberPet.py
 
 
